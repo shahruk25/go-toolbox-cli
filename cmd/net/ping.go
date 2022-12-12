ba80fd77e3d2886a6a99e45bf88b73150acd8a34
shahruk25 <shahruk25@gmail.com>
2022-12-12 23:41:55 +0530

    complete ping cmd

--- cmd/net/ping.go
+++ cmd/net/ping.go
@@ -6,12 +6,32 @@ package net
 import (
 	"fmt"
 	"net/http"
+	"time"
+
 	"github.com/spf13/cobra"
 )
 
 var (
 	urlPath string
+	client  = http.Client{
+		Timeout: 2 * time.Second,
+	}
 )
+
+func ping(domain string) (int, error) {
+	url := "http://" + domain
+	req, err := http.NewRequest("HEAD", url, nil)
+	if err != nil {
+		return 0, err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode, nil
+}
 
 // pingCmd represents the ping command
 var pingCmd = &cobra.Command{
@@ -20,12 +40,13 @@ var pingCmd = &cobra.Command{
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("ping called")
-		// Logic 
-		var client := http.Client{
-			Transport : &http.Transport {
-				Dail:net.Dailer{Timeout: 2 * time.Second}.Dial,
-			}
+		// Logic
+		if resp, err := ping(urlPath); err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(resp)
 		}
+
 	},
 }
 
--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,14 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"os"
 
+	"github.com/shahruk25/go-toolbox-cli/cmd/net"
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -36,6 +34,10 @@ func Execute() {
 	}
 }
 
+func addSubcommandPalettes() {
+	rootCmd.AddCommand(net.NetCmd)
+}
+
 func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
@@ -46,6 +48,5 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	addSubcommandPalettes()
 }
-
-
